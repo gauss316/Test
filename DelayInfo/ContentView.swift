1e0542f30e068bdec14479638e2ee0e8ffea14a0
Takuya Ishizaka <gauss0316larc@gmail.com>
2019-10-17 18:00:06 +0900

    Initial Commit

---
+++ DelayInfo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DelayInfo
+//
+//  Created by 石坂拓哉 on 2019/10/17.
+//  Copyright © 2019 Takuya Ishizaka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
